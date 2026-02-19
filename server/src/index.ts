a59df5d5e12f52f535afa6d8dfadf48aafd1d7fc
Muhammad Asif <asifahmadd53@gmail.com>
2026-02-19 19:22:47 +0500

    Set up database configuration, environment variables, and initial server structure

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,19 @@
 import express from 'express'
-
+import { ENV } from './config/env'
+import cors from "cors";
 const app = express()
+
+
+app.use(cors({ origin: ENV.FRONTEND_URL, credentials: true }));
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+
+app.get('/', (req, res)=>{
+    res.json({message: "Hello from the server"})
+})
+
+app.listen(ENV.PORT, ()=>{
+    console.log(`Server is running at port ${ENV.PORT}`)
+})
